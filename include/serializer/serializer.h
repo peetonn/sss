94e3eb9ba85858e6e1b7b1de6e37950f7de7763c
peetonn <gpeetonn@gmail.com>
2025-01-08 11:51:46 +0100

    chore: first

---
+++ include/serializer/serializer.h
@@ -0,0 +1,304 @@
+/*
+ * Created on Wed Jan 08 2025
+ *
+ * Author: Peter Gusev
+ * Copyright (c) 2025 Peter Gusev. All rights reserved.
+ */
+
+#ifndef __SERIALIZER_H__
+#define __SERIALIZER_H__
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define S_MAX_FIELDS (128)
+
+typedef enum {
+    FIELD_TYPE_INVALID = 0,
+
+    FIELD_TYPE_INT8,
+    FIELD_TYPE_UINT8,
+    FIELD_TYPE_INT16,
+    FIELD_TYPE_UINT16,
+    FIELD_TYPE_INT32,
+    FIELD_TYPE_UINT32,
+    FIELD_TYPE_INT64,
+    FIELD_TYPE_UINT64,
+
+    FIELD_TYPE_FLOAT,
+    FIELD_TYPE_DOUBLE,
+
+    FIELD_TYPE_STRING,
+    FIELD_TYPE_BOOL,
+
+    FIELD_TYPE_BLOB, // continuous memory block
+
+    // FIELD_TYPE_ARRAY,  // fixed size arrays (C arrays)
+    // FIELD_TYPE_VECTOR, // dynamic arrays (C pointer)
+
+    FIELD_TYPE_STRUCT,
+} s_field_type;
+
+typedef enum {
+    TLV_TAG_FIELD = 0x01,
+    TLV_TAG_COMPRESSED_FIELD,
+    TLV_TAG_ENCRYPTED_FIELD,
+    TLV_TAG_TLV_ELEMENT,
+    TLV_TAG_COMPRESSED_TLV,
+    TLV_TAG_ENCRYPTED_TLV,
+} tlv_tag;
+
+// Serialization formats
+typedef enum {
+    FORMAT_BINARY = 1,
+    FORMAT_JSON = 2,
+} serialization_format;
+
+typedef enum {
+    S_FIELD_OPT_NONE = 0,
+    S_FIELD_OPT_OPTIONAL = 1 << 0,
+    S_FIELD_OPT_COMPRESSED = 1 << 1,
+    S_FIELD_OPT_ENCRYPTED = 1 << 2,
+    S_FIELD_OPT_ARRAY = 1 << 3,
+} s_field_opts;
+
+struct s_type_info;
+
+typedef struct {
+    const char* name;
+    const char* label;
+    s_field_type type;
+    size_t offset;
+    size_t size;
+    s_field_opts opts;
+    struct {
+        struct s_type_info* struct_type_info;
+        struct {
+            s_field_type tag_type;
+            size_t tag_offset;
+            int tag_value_int;
+            const char* tag_value_string;
+        } optional_field_info;
+    };
+} s_field_info;
+
+typedef struct {
+    const char* type_name;
+    s_field_info* fields;
+    size_t field_count;
+    size_t type_size;
+} s_type_info;
+
+typedef enum {
+    SERIALIZER_OK = 0,
+    SERIALIZER_ERROR_BUFFER_TOO_SMALL = -1,
+    SERIALIZER_ERROR_INVALID_TYPE = -2,
+    SERIALIZER_ERROR_COMPRESSION_FAILED = -3,
+} s_serializer_error;
+
+typedef void*(s_allocator_allocate) (size_t);
+typedef void*(s_allocator_deallocate) (void*);
+
+typedef struct {
+    s_allocator_allocate* allocate;
+    s_allocator_deallocate* deallocate;
+} s_allocator;
+
+s_serializer_error serialize(const s_type_info* info, const void* data,
+                             serialization_format format, uint8_t* buffer,
+                             size_t buffer_size, size_t* bytes_written);
+
+s_serializer_error deserialize(const s_type_info* info, void* data,
+                               serialization_format format,
+                               const uint8_t* buffer, size_t buffer_size,
+                               s_allocator* allocator);
+
+#define S_GET_STRUCT_TYPE_INFO(TYPE) s_get_struct_type_info_##TYPE()
+#define S_SERIALIZE_BEGIN(TYPE)                         \
+    s_type_info* s_get_struct_type_info_##TYPE() {      \
+        static s_type_info info = {0};                  \
+        static bool initialized = false;                \
+        if (initialized)                                \
+            return &info;                               \
+        info.type_name = #TYPE;                         \
+        static s_field_info fields[S_MAX_FIELDS] = {0}; \
+        info.fields = fields;                           \
+        info.field_count = 0;                           \
+        typedef TYPE struct_type;                       \
+        struct_type dummy;
+
+#define S_SERIALIZE_END() \
+    initialized = true;   \
+    return &info;         \
+    }
+
+#define S_FIELD_LABELED(NAME, TYPE, LABEL)                        \
+    assert(info.field_count < S_MAX_FIELDS && "Too many fields"); \
+    fields[info.field_count++] =                                  \
+        (s_field_info) {.name = #NAME,                            \
+                        .label = LABEL,                           \
+                        .type = TYPE,                             \
+                        .offset = offsetof(struct_type, NAME),    \
+                        .size = sizeof(dummy.NAME),               \
+                        .opts = S_FIELD_OPT_NONE,                 \
+                        .struct_type_info = NULL};
+
+#define S_ASSERT_TYPE(TYPE, VALUE) ((TYPE*) {0} = &(VALUE))
+
+#define GET_MACRO(_1, _2, NAME, ...) NAME
+
+#define S_FIELD_INT8_LABELED(NAME, FIELD_LABEL, ...) \
+    S_ASSERT_TYPE(int8_t, dummy.NAME);               \
+    S_FIELD_LABELED(NAME, FIELD_TYPE_INT8, FIELD_LABEL);
+#define S_FIELD_INT8(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_INT8_LABELED, \
+              S_FIELD_INT8_LABELED)(__VA_ARGS__, NULL)
+
+#define S_FIELD_UINT8_LABELED(NAME, FIELD_LABEL, ...) \
+    S_ASSERT_TYPE(uint8_t, dummy.NAME);               \
+    S_FIELD_LABELED(NAME, FIELD_TYPE_UINT8, FIELD_LABEL);
+#define S_FIELD_UINT8(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_UINT8_LABELED, \
+              S_FIELD_UINT8_LABELED)(__VA_ARGS__, NULL)
+
+#define S_FIELD_INT16_LABELED(NAME, FIELD_LABEL, ...) \
+    S_ASSERT_TYPE(int16_t, dummy.NAME);               \
+    S_FIELD_LABELED(NAME, FIELD_TYPE_INT16, FIELD_LABEL);
+#define S_FIELD_INT16(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_INT16_LABELED, \
+              S_FIELD_INT16_LABELED)(__VA_ARGS__, NULL)
+
+#define S_FIELD_UINT16_LABELED(NAME, FIELD_LABEL, ...) \
+    S_ASSERT_TYPE(uint16_t, dummy.NAME);               \
+    S_FIELD_LABELED(NAME, FIELD_TYPE_UINT16, FIELD_LABEL);
+#define S_FIELD_UINT16(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_UINT16_LABELED, \
+              S_FIELD_UINT16_LABELED)(__VA_ARGS__, NULL)
+
+#define S_FIELD_INT32_LABELED(NAME, FIELD_LABEL, ...) \
+    S_ASSERT_TYPE(int32_t, dummy.NAME);               \
+    S_FIELD_LABELED(NAME, FIELD_TYPE_INT32, FIELD_LABEL);
+#define S_FIELD_INT32(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_INT32_LABELED, \
+              S_FIELD_INT32_LABELED)(__VA_ARGS__, NULL)
+
+#define S_FIELD_UINT32_LABELED(NAME, FIELD_LABEL, ...) \
+    S_ASSERT_TYPE(uint32_t, dummy.NAME);               \
+    S_FIELD_LABELED(NAME, FIELD_TYPE_UINT32, FIELD_LABEL);
+#define S_FIELD_UINT32(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_UINT32_LABELED, \
+              S_FIELD_UINT32_LABELED)(__VA_ARGS__, NULL)
+
+#define S_FIELD_INT64_LABELED(NAME, FIELD_LABEL, ...) \
+    S_ASSERT_TYPE(int64_t, dummy.NAME);               \
+    S_FIELD_LABELED(NAME, FIELD_TYPE_INT64, FIELD_LABEL);
+#define S_FIELD_INT64(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_INT64_LABELED, \
+              S_FIELD_INT64_LABELED)(__VA_ARGS__, NULL)
+
+#define S_FIELD_UINT64_LABELED(NAME, FIELD_LABEL, ...) \
+    S_ASSERT_TYPE(uint64_t, dummy.NAME);               \
+    S_FIELD_LABELED(NAME, FIELD_TYPE_UINT64, FIELD_LABEL);
+#define S_FIELD_UINT64(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_UINT64_LABELED, \
+              S_FIELD_UINT64_LABELED)(__VA_ARGS__, NULL)
+
+#define S_FIELD_FLOAT_LABELED(NAME, FIELD_LABEL, ...) \
+    S_ASSERT_TYPE(float, dummy.NAME);                 \
+    S_FIELD_LABELED(NAME, FIELD_TYPE_FLOAT, FIELD_LABEL);
+#define S_FIELD_FLOAT(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_FLOAT_LABELED, \
+              S_FIELD_FLOAT_LABELED)(__VA_ARGS__, NULL)
+
+#define S_FIELD_DOUBLE_LABELED(NAME, FIELD_LABEL, ...) \
+    S_ASSERT_TYPE(double, dummy.NAME);                 \
+    S_FIELD_LABELED(NAME, FIELD_TYPE_DOUBLE, FIELD_LABEL);
+#define S_FIELD_DOUBLE(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_DOUBLE_LABELED, \
+              S_FIELD_DOUBLE_LABELED)(__VA_ARGS__, NULL)
+
+#define S_FIELD_STRING_LABELED(NAME, FIELD_LABEL, ...) \
+    S_ASSERT_TYPE(char*, dummy.NAME);                  \
+    S_FIELD_LABELED(NAME, FIELD_TYPE_STRING, FIELD_LABEL);
+#define S_FIELD_STRING(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_STRING_LABELED, \
+              S_FIELD_STRING_LABELED)(__VA_ARGS__, NULL)
+
+#define S_FIELD_BOOL_LABELED(NAME, FIELD_LABEL, ...) \
+    S_ASSERT_TYPE(bool, dummy.NAME);                 \
+    S_FIELD_LABELED(NAME, FIELD_TYPE_BOOL, FIELD_LABEL);
+#define S_FIELD_BOOL(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_BOOL_LABELED, \
+              S_FIELD_BOOL_LABELED)(__VA_ARGS__, NULL)
+
+#define S_FIELD_BLOB_LABELED(NAME, FIELD_LABEL, ...) \
+    S_FIELD_LABELED(NAME, FIELD_TYPE_BLOB, FIELD_LABEL);
+
+#define S_FIELD_BLOB(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_BLOB_LABELED, \
+              S_FIELD_BLOB_LABELED)(__VA_ARGS__, NULL)
+
+#define S_FIELD_STRUCT_LABELED(NAME, TYPE, FIELD_LABEL, ...)      \
+    assert(info.field_count < S_MAX_FIELDS && "Too many fields"); \
+    fields[info.field_count++] =                                  \
+        (s_field_info) {.name = #NAME,                            \
+                        .label = FIELD_LABEL,                     \
+                        .type = FIELD_TYPE_STRUCT,                \
+                        .offset = offsetof(struct_type, NAME),    \
+                        .size = sizeof(dummy.NAME),               \
+                        .opts = S_FIELD_OPT_NONE,                 \
+                        .struct_type_info = S_GET_STRUCT_TYPE_INFO(TYPE)};
+
+#define S_FIELD_STRUCT(...)                        \
+    GET_MACRO(__VA_ARGS__, S_FIELD_STRUCT_LABELED, \
+              S_FIELD_STRUCT_LABELED)(__VA_ARGS__, NULL)
+
+#define S_UNION_BEGIN_TAG(NAME, TAG_NAME)                  \
+    {                                                      \
+        size_t union_start_field_index = info.field_count; \
+        size_t tag_offset = offsetof(struct_type, TAG_NAME);
+
+#define S_UNION_END()                                                     \
+    for (size_t i = union_start_field_index; i < info.field_count; i++) { \
+        info.fields[i].opts |= S_FIELD_OPT_OPTIONAL;                      \
+    }                                                                     \
+    }
+
+#define S_FIELD_TAGGED_INT(NAME, TAG_VALUE)                                   \
+    {                                                                         \
+        bool field_found = false;                                             \
+        for (size_t i = union_start_field_index; i < info.field_count; i++) { \
+            if (strcmp(info.fields[i].name, #NAME) == 0) {                    \
+                field_found = true;                                           \
+                info.fields[i].opts |= S_FIELD_OPT_OPTIONAL;                  \
+                info.fields[i].optional_field_info.tag_offset = tag_offset;   \
+                info.fields[i].optional_field_info.tag_value_int = TAG_VALUE; \
+                info.fields[i].optional_field_info.tag_type =                 \
+                    FIELD_TYPE_INT32;                                         \
+                break;                                                        \
+            }                                                                 \
+        }                                                                     \
+        assert(field_found && "Field " #NAME " not found in union");          \
+    }
+
+#define S_FIELD_TAGGED_STRING(NAME, TAG_VALUE)                                \
+    {                                                                         \
+        bool field_found = false;                                             \
+        for (size_t i = union_start_field_index; i < info.field_count; i++) { \
+            if (strcmp(info.fields[i].name, #NAME) == 0) {                    \
+                field_found = true;                                           \
+                info.fields[i].opts |= S_FIELD_OPT_OPTIONAL;                  \
+                info.fields[i].optional_field_info.tag_offset = tag_offset;   \
+                info.fields[i].optional_field_info.tag_value_string =         \
+                    TAG_VALUE;                                                \
+                info.fields[i].optional_field_info.tag_type =                 \
+                    FIELD_TYPE_STRING;                                        \
+                break;                                                        \
+            }                                                                 \
+        }                                                                     \
+        assert(field_found && "Field " #NAME " not found in union");          \
+    }
+
+#endif
---
+++ src/serializer.c
@@ -0,0 +1,264 @@
+/*
+ * Created on Wed Jan 08 2025
+ *
+ * Author: Peter Gusev
+ * Copyright (c) 2025 Peter Gusev. All rights reserved.
+ */
+
+#include "serializer/serializer.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+int is_field_present(const void* struct_data, const s_field_info* field) {
+    if (field->opts & S_FIELD_OPT_OPTIONAL) {
+        void* tag = (void*) ((uint8_t*) struct_data +
+                             field->optional_field_info.tag_offset);
+
+        if (field->optional_field_info.tag_type == FIELD_TYPE_INT32) {
+            if (*(int32_t*) tag != field->optional_field_info.tag_value_int) {
+                return 0;
+            }
+        } else {
+            if (strcmp((char*) tag,
+                       field->optional_field_info.tag_value_string) != 0) {
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
+static s_serializer_error serialize_field(const s_field_info* field,
+                                          const void* data,
+                                          serialization_format format,
+                                          uint8_t* buffer, size_t buffer_size,
+                                          size_t* bytes_written) {
+    const uint8_t* field_data = (const uint8_t*) data + field->offset;
+
+    // check if field is optional and if it should be serialized
+    if (is_field_present(data, field) == 0) {
+        *bytes_written = 0;
+        return SERIALIZER_OK;
+    }
+
+    switch (field->type) {
+    case FIELD_TYPE_INT32:
+    case FIELD_TYPE_UINT32:
+        if (buffer_size < sizeof(uint32_t)) {
+            return SERIALIZER_ERROR_BUFFER_TOO_SMALL;
+        }
+        memcpy(buffer, field_data, sizeof(uint32_t));
+        *bytes_written = sizeof(uint32_t);
+        break;
+
+    case FIELD_TYPE_FLOAT:
+        if (buffer_size < sizeof(float)) {
+            return SERIALIZER_ERROR_BUFFER_TOO_SMALL;
+        }
+        memcpy(buffer, field_data, sizeof(float));
+        *bytes_written = sizeof(float);
+        break;
+
+    case FIELD_TYPE_BOOL:
+        if (buffer_size < sizeof(bool)) {
+            return SERIALIZER_ERROR_BUFFER_TOO_SMALL;
+        }
+        memcpy(buffer, field_data, sizeof(bool));
+        *bytes_written = sizeof(bool);
+        break;
+
+    case FIELD_TYPE_STRING: {
+        const char* str = *(const char**) field_data;
+        size_t str_len = strlen(str) + 1; // Include null terminator
+        if (buffer_size < str_len) {
+            return SERIALIZER_ERROR_BUFFER_TOO_SMALL;
+        }
+        memcpy(buffer, str, str_len);
+        *bytes_written = str_len;
+        break;
+    }
+
+    case FIELD_TYPE_BLOB: {
+        if (buffer_size < field->size) {
+            return SERIALIZER_ERROR_BUFFER_TOO_SMALL;
+        }
+        memcpy(buffer, field_data, field->size);
+        *bytes_written = field->size;
+        break;
+    }
+
+    case FIELD_TYPE_STRUCT: {
+        const s_type_info* sub_info = field->struct_type_info;
+        if (!sub_info) {
+            return SERIALIZER_ERROR_INVALID_TYPE;
+        }
+
+        s_serializer_error err = serialize(sub_info, field_data, format, buffer,
+                                           buffer_size, bytes_written);
+        if (err != SERIALIZER_OK) {
+            return err;
+        }
+        break;
+    }
+
+    default:
+        return SERIALIZER_ERROR_INVALID_TYPE;
+    }
+
+    return SERIALIZER_OK;
+}
+
+s_serializer_error serialize(const s_type_info* info, const void* data,
+                             serialization_format format, uint8_t* buffer,
+                             size_t buffer_size, size_t* bytes_written) {
+    if (!info || !data || !buffer || !bytes_written) {
+        return SERIALIZER_ERROR_INVALID_TYPE;
+    }
+
+    size_t total_bytes = 0;
+    uint8_t* current_buffer = buffer;
+    size_t remaining_size = buffer_size;
+
+    // Serialize each field
+    for (size_t i = 0; i < info->field_count; i++) {
+        size_t field_bytes = 0;
+        s_serializer_error err =
+            serialize_field(&info->fields[i], data, format, current_buffer,
+                            remaining_size, &field_bytes);
+
+        if (err != SERIALIZER_OK) {
+            return err;
+        }
+
+        current_buffer += field_bytes;
+        remaining_size -= field_bytes;
+        total_bytes += field_bytes;
+    }
+
+    *bytes_written = total_bytes;
+    return SERIALIZER_OK;
+}
+
+static s_serializer_error
+deserialize_field(const s_field_info* field, void* data,
+                  serialization_format format, const uint8_t* buffer,
+                  size_t buffer_size, size_t* bytes_read) {
+    uint8_t* field_data = (uint8_t*) data + field->offset;
+
+    // check if field is optional and if it should be deserialized
+    if (is_field_present(data, field) == 0) {
+        *bytes_read = 0;
+        return SERIALIZER_OK;
+    }
+
+    switch (field->type) {
+    case FIELD_TYPE_INT32:
+    case FIELD_TYPE_UINT32:
+        if (buffer_size < sizeof(uint32_t)) {
+            return SERIALIZER_ERROR_BUFFER_TOO_SMALL;
+        }
+        memcpy(field_data, buffer, sizeof(uint32_t));
+        *bytes_read = sizeof(uint32_t);
+        break;
+
+    case FIELD_TYPE_FLOAT:
+        if (buffer_size < sizeof(float)) {
+            return SERIALIZER_ERROR_BUFFER_TOO_SMALL;
+        }
+        memcpy(field_data, buffer, sizeof(float));
+        *bytes_read = sizeof(float);
+        break;
+
+    case FIELD_TYPE_BOOL:
+        if (buffer_size < sizeof(bool)) {
+            return SERIALIZER_ERROR_BUFFER_TOO_SMALL;
+        }
+        memcpy(field_data, buffer, sizeof(bool));
+        *bytes_read = sizeof(bool);
+        break;
+
+    case FIELD_TYPE_STRING: {
+        // Find string length (including null terminator)
+        size_t str_len = 0;
+        while (str_len < buffer_size && buffer[str_len] != '\0') {
+            str_len++;
+        }
+        if (str_len >= buffer_size) { // No null terminator found
+            return SERIALIZER_ERROR_BUFFER_TOO_SMALL;
+        }
+        str_len++; // Include null terminator
+
+        // Allocate and copy string
+        char* str = malloc(str_len);
+        if (!str) {
+            return SERIALIZER_ERROR_BUFFER_TOO_SMALL; // Could add a new error
+                                                      // type
+        }
+        memcpy(str, buffer, str_len);
+        *(char**) field_data = str; // Store pointer to new string
+        *bytes_read = str_len;
+        break;
+    }
+
+    case FIELD_TYPE_BLOB: {
+        if (buffer_size < field->size) {
+            return SERIALIZER_ERROR_BUFFER_TOO_SMALL;
+        }
+        memcpy(field_data, buffer, field->size);
+        *bytes_read = field->size;
+        break;
+    }
+
+    case FIELD_TYPE_STRUCT: {
+        const s_type_info* sub_info = field->struct_type_info;
+        if (!sub_info) {
+            return SERIALIZER_ERROR_INVALID_TYPE;
+        }
+
+        s_serializer_error err = deserialize(sub_info, field_data, format,
+                                             buffer, buffer_size, NULL);
+        if (err != SERIALIZER_OK) {
+            return err;
+        }
+
+        *bytes_read = buffer_size;
+        break;
+    }
+
+    default:
+        return SERIALIZER_ERROR_INVALID_TYPE;
+    }
+
+    return SERIALIZER_OK;
+}
+
+s_serializer_error deserialize(const s_type_info* info, void* data,
+                               serialization_format format,
+                               const uint8_t* buffer, size_t buffer_size,
+                               s_allocator* allocator) {
+    if (!info || !data || !buffer) {
+        return SERIALIZER_ERROR_INVALID_TYPE;
+    }
+
+    const uint8_t* current_buffer = buffer;
+    size_t remaining_size = buffer_size;
+
+    // Deserialize each field
+    for (size_t i = 0; i < info->field_count; i++) {
+        size_t bytes_read = 0;
+        s_serializer_error err =
+            deserialize_field(&info->fields[i], data, format, current_buffer,
+                              remaining_size, &bytes_read);
+
+        if (err != SERIALIZER_OK) {
+            return err;
+        }
+
+        current_buffer += bytes_read;
+        remaining_size -= bytes_read;
+    }
+
+    return SERIALIZER_OK;
+}
